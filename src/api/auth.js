173fd5cf98b408bc1ecdc230fe2236abb4d4445d
NELSON RIVERA <nrrvrivera@hotmail.com>
2024-10-03 14:15:01 -0600

    mensajes de ayuda 1

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
- export const API = "http://localhost:4000/api";
-// export const API = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/api";
+// export const API = "http://localhost:4000/api";
+ export const API = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/api";
 
 axios.defaults.withCredentials = true;
 
